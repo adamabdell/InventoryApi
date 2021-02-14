f7ff5f37d7354239cf5334212fe47b2dbc5e2483
Adam Abdellaoui <adam.abd13@gmail.com>
2021-02-14 15:17:35 -0500

    Added getAllItems func and modified insertItem func

--- api/dbOperations.js
+++ api/dbOperations.js
@@ -1,12 +1,14 @@
 module.exports = {
     getItem: getItem,
-    insertItem: insertItem
+    insertItem: insertItem,
+    getAllItems: getAllItems
 };
 
 
+//retrieve one item that was selected
 async function getItem(db, itemName){
 
-    const query = {name: itemName};
+    const query = {foodName: itemName};
 
    try{ 
         const result = await db.collection('items').find(query).toArray();
@@ -16,16 +18,44 @@ async function getItem(db, itemName){
    }
 }
 
-async function insertItem(db, itemName, itemCount){
+//retireve all items from DB
+async function getAllItems(db){
+
+    const alphabetical = { foodName: 1 };
+
+    try{
+        const result = await db.collection('items').find({}).sort(alphabetical).toArray();
+        console.log(result);
+   } catch(err) {
+       throw err;
+   }
+}
+
+
+async function insertItem(db, itemName, itemCount, out){
 
     const item = {
-        name: itemName,
-        count: itemCount
+        foodName: itemName,
+        quantity: itemCount
     };
 
     try{
-        await db.collection('items').insertOne(item);
-        console.log(itemCount + ' '+itemName+ ' inserted into DB');
+        await db.collection('items').count({foodName: itemName})
+            .then((count) => {
+                if(count>0) {
+                    if(!out){
+                        console.log('EXISTS');
+                        db.collection('items').update({foodName: itemName}, { $inc: { quantity: itemCount}});
+                    }else{
+                        db.collection('items').update({foodName: itemName}, { $inc: { quantity: (itemCount*-1)}});
+                    }   
+                }else{
+                    console.log("ENTER");
+                    db.collection('items').insertOne(item);
+                }
+
+            });            
+//        console.log(itemCount + ' '+itemName+ ' inserted into DB');
     }catch(err){
         throw err;
     }
